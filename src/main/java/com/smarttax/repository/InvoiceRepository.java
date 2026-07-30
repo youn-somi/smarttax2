4e7f113ab2e013dfe8c777a374eb7f4000753b81
kimheajeong <kimheajeong0510@gmail.com>
2026-07-30 20:33:02 +0900

    feat: 발행일 조회 기능 추가

--- src/main/java/com/smarttax/repository/InvoiceRepository.java
+++ src/main/java/com/smarttax/repository/InvoiceRepository.java
@@ -4,13 +4,14 @@ package com.smarttax.repository;
 import com.smarttax.entity.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface InvoiceRepository
         extends JpaRepository<Invoice, Long> {
     List<Invoice> findBysupplierName(String supplierName);
     List<Invoice> findByCustomerName(String customerName);
-
+    List<Invoice> findByIssueDate(LocalDate issueDate);
 }
 
 
